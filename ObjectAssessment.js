003e4e8af5d68559fba92ae2d3907980badf3ebc
Catherine J <cjarocki@gmail.com>
2017-02-04 16:08:30 -0600

    Update ObjectAssessment.js

--- ObjectAssessment.js
+++ ObjectAssessment.js
@@ -1,6 +1,6 @@
-avengersMovie = {
-	name : "Avengers";
-	runtimeMinutes : 143;
-	releaseYear; 2012;
-	director: "Joss Whedon".
+var avengersMovie = {
+	movieName: "Avengers", 
+	runtimeMinutes: 143, 
+	releaseYear: 2012, 
+	director: "Joss Whedon"
 };
